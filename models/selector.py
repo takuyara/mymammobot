eb49fb71ad1b415eff391b04becca8efc77502cb
takuyara <takuyara2136@gmail.com>
2023-07-15 22:14:21 +0100

    Different weights for rotation and translation in final selection

--- models/selector.py
+++ models/selector.py
@@ -3,13 +3,17 @@ from torch import nn
 from models.model_utils import get_mlp
 
 class MLPSelector(nn.Module):
-	def __init__(self, in_channels, num_neurons, dropout, num_inputs):
+	def __init__(self, in_channels, num_neurons, dropout, num_inputs, output_dim):
 		super(MLPSelector, self).__init__()
 		self.mlp, prev_features = get_mlp(in_channels, num_neurons, dropout)
-		self.fc = nn.Sequential(nn.Linear(prev_features, num_inputs), nn.Softmax(dim = -1))
+		self.fc = nn.Linear(prev_features, output_dim * num_inputs)
+		self.num_inputs = num_inputs
+		self.output_dim = output_dim
 	def forward(self, x, *v):
 		weights = self.mlp(x)
 		weights = self.fc(weights)
 		v = torch.stack(v, dim = -1)
+		weights = weights.view(weights.size(0), self.output_dim, self.num_inputs)
+		weights = nn.Softmax(dim = -1)(weights)
 		out = torch.sum(v * weights, dim = -1)
 		return out
