53a18549a6b3b143477a09221f23f4cb50145cdb
takuyara <takuyara2136@gmail.com>
2023-08-30 16:29:39 +0100

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -67,8 +67,9 @@ def get_img_transform(data_stats_path, method, n_channels, train):
 			q = q / len(q)
 			q = q.reshape(orig_shape)
 			if train:
-				#sigma_blur = randu_gen(0.1, 2.3)()
-				sigma_intensity = 0.1
+				#sigma_blur = randu_gen(2.0, 2.3)()
+				sigma_blur = 2.0
+				sigma_intensity = 0.2
 				q = q + np.random.randn(*q.shape) * sigma_intensity
 				q = ndimage.gaussian_filter(q, sigma = sigma_blur)
 			q = torch.tensor(q).float().unsqueeze(0).repeat(n_channels, 1, 1)
